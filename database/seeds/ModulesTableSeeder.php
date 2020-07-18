0bd559a49772584adf11b00d4b71ec983a8616a5
Eliza Ahmed <elizarafee@gmail.com>
2020-07-19 00:14:04 +0600

    seeder for modules table created

---
+++ database/seeds/ModulesTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ModulesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $modules = [
+            'Matter Management',
+            'Legal Billing',
+            'Documents',
+            'Time and Expense Recording',
+            'Legal Calendaring',
+            'Client Accounting',
+            'Firm Dashboard',
+            'Integrations',
+            'Security',
+            'Mobile App',
+            'Support Team',
+        ];
+
+        $features = array();
+
+        foreach ($modules as $feature) {
+            $features[] = [
+                'name' => ucfirst($feature),
+                'created_at' =>  \Carbon\Carbon::now(),
+            ];
+        }
+    
+        DB::table('modules')->insert($features);
+    }
+}
